55244419affa75cc62d3ede1cd1c782e53b8805d
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2026-05-09 12:10:09 -0300

    chore(build): add ModelMapper dependency and mapping configuration

---
+++ src/main/java/com/outforce/coupon/infrastructure/config/MyModelMapper.java
@@ -0,0 +1,21 @@
+package com.outforce.coupon.infrastructure.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MyModelMapper {
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public <S, D> D mapTo(S source, Class<D> destClass) {
+        return MODEL_MAPPER.map(source, destClass);
+    }
+
+    public <D, T> List<D> toList(List<T> entityList, Class<D> outClass) {
+        return entityList.stream()
+                .map(entity -> MODEL_MAPPER.map(entity, outClass))
+                .toList();
+    }
+}
